638a1fda9c1c9dfade513d31b103d2783bb90f99
big-lark <81342631+big-lark@users.noreply.github.com>
2023-10-13 14:53:21 +0900

    update

--- main.py
+++ main.py
@@ -2,4 +2,23 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+@app.get("/compare")
+def read_root():
+    return {"Hello": "비교정보를!!"}
+
+@app.get("/compare/home1")
+def read_root():
+    return {"Hello": "비교정보를!!home1"}
+
+@app.get("/compare/home2")
+def read_root():
+    return {"Hello": "비교정보를!!home2"}
+
+@app.get("/air")
+def read_root():
+    return {"Hello": "날씨정보를!"}
+
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
+
